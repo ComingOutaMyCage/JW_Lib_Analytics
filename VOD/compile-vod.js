78831d5b693dc3230384ea3e3fe363bcf7d4c536
ComingOuta MyCage <jw.paradise.now@gmail.com>
2022-12-05 15:55:02 +1000

    Fixed touch handling on map clusters

--- VOD/compile-vod.js
+++ VOD/compile-vod.js
@@ -9,6 +9,7 @@ function sanitize(input, replacement) {
     const reservedRe = /^\.+$/;
     const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
     var sanitized = input
+        .replace("#", "")
         .replace(illegalRe, replacement)
         .replace(controlRe, replacement)
         .replace(reservedRe, replacement)
@@ -34,7 +35,7 @@ const vttToPlainText = (vttCaption) => {
     lines = lines.filter(line => line.length > 0);
     lines = lines.filter((line, index, lines) => line !== lines[index + 1]);
 
-    return lines.join('\n');
+    return lines.join('\r\n');
 }
 
 const downloadFile = (async (url, path) => {
@@ -55,72 +56,79 @@ var videosByCategory = JSON.parse(contents);
 
 
 let keysDone = {};
-fs.rmdirSync(`./VOD-Info/`, { recursive: true, force: true });
-fs.rmdirSync(`./VOD-Subtitles/`, { recursive: true, force: true });
+//fs.rmdirSync(`./VOD-Info/`, { recursive: true, force: true });
+//fs.rmdirSync(`./VOD-Subtitles/`, { recursive: true, force: true });
+async function DownloadAll() {
+    for (const category of Object.keys(videosByCategory)) {
+        for (const media of Object.values(videosByCategory[category])) {
+            let info = {...media}
+            delete info.availableLanguages;
+            delete info.images;
+            delete info.tags;
+            delete info.title;
+            info.files = media.files.filter(f => f.progressiveDownloadURL && f.progressiveDownloadURL.endsWith('.mp4'));
+            let subtitlesUrls = media.files.filter(f => f.subtitles && f.subtitles.url).map(f => f.subtitles.url);
+            if (subtitlesUrls.length === 0) continue;
+            info.subtitlesUrl = subtitlesUrls[0];
 
-Object.keys(videosByCategory).forEach(async category => {
-    videosByCategory[category].forEach(async (media) => {
-        let info = { ...media }
-        delete info.availableLanguages;
-        delete info.images;
-        delete info.tags;
-        delete info.title;
-        info.files = media.files.filter(f => f.progressiveDownloadURL && f.progressiveDownloadURL.endsWith('.mp4'));
-        let subtitlesUrls = media.files.filter(f => f.subtitles && f.subtitles.url).map(f => f.subtitles.url);
-        if (subtitlesUrls.length == 0) return;
-        info.subtitlesUrl = subtitlesUrls[0];
+            let naturalKey = media.naturalKey;
+            let datePublished = new Date(info.firstPublished);
+            let year = datePublished.getFullYear();
+            let category = media.primaryCategory;
+            let title = media.title;
+            let title_safe = sanitize(title.replace(/[\’\‘\“\”\"]/g, "'").replace(/(\d):(\d)/g, '$1.$2').replace(/(\:\s*|\—)/g, '-').replace(/[\?\!\'\"]/g, ""), '_');
 
-        let naturalKey = media.naturalKey;
-        let datePublished = new Date(info.firstPublished);
-        let year = datePublished.getFullYear();
-        let category = media.primaryCategory;
-        let title = media.title;
-        let title_safe = sanitize(title.replace(/[\’\‘\“\”\"]/g, "'").replace(/(\d):(\d)/g, '$1.$2').replace(/(\:\s*|\—)/g, '-').replace(/[\?\!\'\"]/g, ""), '_');
+            info.Name = media.naturalKey;
+            info.Title = media.title;
+            info.UndatedReferenceTitle = media.title;
+            info.ShortTitle = null;
+            info.Category = "vod";
+            info.Year = year;
+            info.Symbol = media.primaryCategory;
 
-        info.Name = media.naturalKey;
-        info.Title = media.title;
-        info.UndatedReferenceTitle = media.title;
-        info.ShortTitle = null;
-        info.Category = "vod";
-        info.Year = year;
-        info.Symbol = media.primaryCategory;
+            let dir = `./VOD-Info/${year}/${category}/${naturalKey}/`;
+            let dir2 = `./VOD-Subtitles/${category}/${title_safe}/`;
 
-        let dir = `./VOD-Info/${year}/${category}/${naturalKey}/`;
-        let dir2 = `./VOD-Subtitles/${category}/${title_safe}/`;
-
-        if (media.naturalKey in keysDone) {
-            console.log("Already processed " + dir);
-            return;
-        }
-        keysDone[media.naturalKey] = true;
-        console.log("Processing " + dir);
-
-        fs.mkdirSync(dir, { recursive: true });
-
-        let subFileName = title_safe;
-        if (subFileName.length > 50)
-            subFileName = title_safe.substring(0, 50);
-        let vttPath = dir + subFileName + ".vtt";
-        let txtPath = dir + subFileName + ".txt";
-        await downloadFile(info.subtitlesUrl, vttPath).then(()=>{
-            let vttContents = fs.readFileSync(vttPath).toString();
-            let txtContents = vttToPlainText(vttContents);
-            if(txtContents == undefined) {
-                console.error("No subtitles for " + vttPath);
-                return;
+            if (media.naturalKey in keysDone) {
+                console.log("Already processed " + dir);
+                continue;
             }
-            fs.writeFileSync(txtPath, txtContents, (err) => {
-                if (err) { console.error(err); return; };
-            });
+            keysDone[media.naturalKey] = true;
+            console.log("Processing " + dir);
 
+            fs.mkdirSync(dir, {recursive: true});
 
-            fs.writeFileSync(dir + "info.json", JSON.stringify(info, null, 4), (err) => {
-                if (err) { console.error(err); return; };
-                console.log(info.Title + " has been saved");
+            let subFileName = title_safe;
+            if (subFileName.length > 50)
+                subFileName = title_safe.substring(0, 50);
+            let vttPath = dir + subFileName + ".vtt";
+            let txtPath = dir + subFileName + ".txt";
+            await downloadFile(info.subtitlesUrl, vttPath).then(() => {
+                let vttContents = fs.readFileSync(vttPath).toString();
+                let txtContents = vttToPlainText(vttContents);
+                if (txtContents == undefined) {
+                    console.error("No subtitles for " + vttPath);
+                    return;
+                }
+                fs.writeFileSync(txtPath, txtContents, (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+
+                fs.writeFileSync(dir + "info.json", JSON.stringify(info, null, 4), (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    console.log(info.Title + " has been saved");
+                });
+
+                fs.mkdirSync(dir2, {recursive: true});
+                fse.copySync(dir, dir2);
             });
-            
-            fs.mkdirSync(dir2, { recursive: true });
-            fse.copySync(dir, dir2);
-        });
-    });
-});
+        }
+    }
+}
+DownloadAll();
