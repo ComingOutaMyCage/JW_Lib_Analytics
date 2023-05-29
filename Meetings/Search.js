343b3f0b3168d13f92f0c5f082ed69968fdee668
ComingOuta MyCage <jw.paradise.now@gmail.com>
2023-05-29 23:30:39 +1000

    Updated map to show schedule

--- Meetings/Search.js
+++ Meetings/Search.js
@@ -1,6 +1,13 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import wc from 'which-country';
+import iso from 'iso-3166-1';
 
+//console.log(wc([-100, 40])); // USA
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 var allMeetings = {};
 var results = [];
@@ -8,27 +15,50 @@ var resultsCSV = "Name\tType\tAddress\tPhone";
 
 
 let stateTotals = {};
+var byCountry = {};
 
-allMeetings = LoadJSON("./Meetings/Meetings.json");
+allMeetings = LoadJSON(__dirname + "/Meetings.json");
 for (const meeting of Object.values(allMeetings)){
-    let stateMatch = meeting.properties.orgName.match(/(\w+) \(USA\)/);
-    if(stateMatch != null){
-        stateTotals[stateMatch[1]] = (stateTotals[stateMatch[1]] ?? 0) + 1;
-    }
+    if(meeting.active) continue;
+    let countryCode = wc([meeting.location.longitude, meeting.location.latitude]);
+    if(countryCode === null) continue;
 
-    if (meeting.properties.orgName.indexOf(" PA (USA)") > 0){
-        results.push(meeting);
-        let phone = '';
-        meeting.properties.phones.forEach(element => {
-            phone += element.phone + " ";
-        });
-        resultsCSV += "\n" + (meeting.properties.orgName + "\t" + meeting.properties.orgType + "\t" + meeting.properties.address.trim() + "\t" + phone.trim()).replaceAll("\n", " ").replaceAll("\r", "");
+    let country = iso.whereAlpha3(countryCode);
+    if(!country || !country.country)
+    {
+        continue;
     }
+    console.log(countryCode, country);
+    country = country.country;
+
+    if(byCountry[country] === undefined)
+        byCountry[country] = 0;
+    byCountry[country]++;
+
+    // let stateMatch = meeting.properties.orgName.match(/(\w+) \(USA\)/);
+    // if(stateMatch != null){
+    //     stateTotals[stateMatch[1]] = (stateTotals[stateMatch[1]] ?? 0) + 1;
+    // }
+    //
+    // if (meeting.properties.orgName.indexOf("(USA)") > 0){
+    //     results.push(meeting);
+    //     let phone = '';
+    //     meeting.properties.phones.forEach(element => {
+    //         phone += element.phone + " ";
+    //     });
+    //     resultsCSV += "\n" + (meeting.properties.orgName + "\t" + meeting.properties.orgType + "\t" + meeting.properties.address.trim() + "\t" + phone.trim()).replaceAll("\n", " ").replaceAll("\r", "");
+    // }
 }
+results = Object.keys(byCountry).sort().reduce(
+    (obj, key) => {
+        obj[key] = byCountry[key];
+        return obj;
+    }, {}
+);
 
-await SaveFile(`./Meetings/Search.json`, JSON.stringify(results, null, 1));
-await SaveFile(`./Meetings/USATotals.json`, JSON.stringify(stateTotals, null, 1));
-await SaveFile(`./Meetings/Search.csv`, resultsCSV);
+await SaveFile(__dirname + `/Search.json`, JSON.stringify(results, null, 1));
+await SaveFile(__dirname + `/USATotals.json`, JSON.stringify(stateTotals, null, 1));
+await SaveFile(__dirname + `/Search.csv`, resultsCSV);
 // SaveAllMeetings(allMeetings);
 // throw '';
 
